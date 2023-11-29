18441d75f22963d6f5477044e7716a6e09982081
Sagarika Sahoo <152247474+Sagarikasahoo24@users.noreply.github.com>
2023-11-29 05:30:51 +0000

    file added

---
+++ 01-Basics/01-Variable.js
@@ -0,0 +1,23 @@
+/*
+1. Variable are container for storing data.
+2. JS variable can be declared in 4 ways.
+        1. Autonamtically
+        2. using var
+        3. using left
+        4. using const
+3. the var key word is used in all js code from 1995 to 2017.
+4. The late and const key word were added to js in 2015.
+5. The var key word should only be used in code written for older browser.
+6. Let os block scope where as var is function scope.
+*/
+const accountID=334;
+let accountEmail="sagarika@gmail.com"
+var accountPassword="12345"
+accountCity="Bhubaneswar"
+let accountState;
+const accountID=334;
+console.log(accountID);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountID,accountEmail,accountPassword])
