be0c0b7b80aa45ded22c2ca7fc9b1eccbc7b17b5
Sagarika Sahoo <152247474+Sagarikasahoo24@users.noreply.github.com>
2023-11-29 04:25:01 +0000

    file added

--- 01-Basics/test.js
+++ 01-Basics/test.js
@@ -1,5 +1,10 @@
 console.log("hello world");
+
 /*
-ejdeijflokgtr;
-krjnvkmldfm 
+1. JavaScript is a high level scripting programming language used on developing web and mobile application efficiency.
+2. We use JS in both client side as well as server side language.
+3  We use js for developing logics inn our cide.
+4. JS was developing in 1997.
+5. Noew these days we used the Esb version  of JS. It was started on 2015 and now it is the latest version of js.
+             Esb-Echma script 6(vanilla javascript) 
 */
